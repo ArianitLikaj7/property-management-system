56cf01c1c254f0d02d81204c95deaf136be66ee7
arianit <arianitlikaj46@gmail.com>
2024-01-17 15:41:23 +0100

    path variable

--- src/main/java/com/mycompany/propertymanagement/controller/CalculatorController.java
+++ src/main/java/com/mycompany/propertymanagement/controller/CalculatorController.java
@@ -1,16 +1,24 @@
 package com.mycompany.propertymanagement.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/calculator")
 public class CalculatorController {
 
-    @GetMapping("/add")
+    @GetMapping("/add")// method level mapping of a url to a controller function
     public Double add(@RequestParam("n1") Double n1, @RequestParam("n2") Double n2){
         return n1 + n2;
+    }
+    @GetMapping("/sub/{n1}/{n2}")// map the values of url to java variables by Path variables
+    public Double substract(@PathVariable("n1") Double n1, @PathVariable("n2") Double n2){
+        Double result = null;
+        if(n1 > n2){
+            result = n1 - n2;
+        }
+        else {
+            result = n2 -n1;
+        }
+        return result;
     }
 }
